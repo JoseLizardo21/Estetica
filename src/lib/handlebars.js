2759221b524bca26990f5f8048f3f8a68dcb82be
Jose <jose.lizardo21.cont@gmail.com>
2023-01-12 19:39:15 -0500

    Se modificó menu

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -2,7 +2,7 @@ const helpersHandlebars = {}
 helpersHandlebars.compareUser = (username)=>{
     if(username === "Admin"){
         const element = '<div class="col">'+
-            '<a href="/createUsers" class="card bg-white text-center">Agregar trabajador</a>'
+            '<a href="/createUsers" class="card text-center btnMenu">Agregar trabajador</a>'
         +'</div>';
         return (element)
     }
