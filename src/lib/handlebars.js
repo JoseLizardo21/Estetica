f2933e2379d169ab41e97f57df4fd01f02c29228
Jose <jose.lizardo21.cont@gmail.com>
2023-01-14 10:50:20 -0500

    Se agregó nuevos iconos al menú

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -2,10 +2,18 @@ const helpersHandlebars = {}
 helpersHandlebars.compareUser = (username)=>{
     if(username === "Admin"){
         const element = '<div class="col">'+
-            '<a href="/createUsers" class="card text-center btnMenu">Agregar trabajador</a>'
-        +'</div>';
+        '<a href="/createUsers" class="card btnMenu">'+
+            '<div class="w-100 h-100 row ps-3 d-flex align-items-center">'+
+                '<img src="/imgs/icons/agregarUsers.png" alt="imagen" class="imgIcons">'+
+                '<div class="col d-flex justify-content-center align-items-center fs-3 text-center">'+
+                    'Agregar Trabajador'+
+                '</div>'+
+            '</div>'+
+        '</a>'+            
+    '</div>';
         return (element)
     }
     return null;
 }
 module.exports = helpersHandlebars;
+
