baa1f066457f530bcca74163674a450c2034dae7
Jose <jose.lizardo21.cont@gmail.com>
2023-01-24 14:12:13 -0500

    se arreglo alguna falla

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,12 @@
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+}
 module.exports = {
     database: {
         host: process.env.HOST,
         user: process.env.USER,
         password: process.env.PASSWORD,
-        database: process.env.DATABASE
+        database: process.env.DATABASE,
+        port: process.env.PUERTO
     }
 }
