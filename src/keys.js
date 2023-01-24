6747112c15b5448e7e4d23a630ae1302f3e8512d
Jose <jose.lizardo21.cont@gmail.com>
2023-01-24 13:59:15 -0500

    se cambió modulo mysql2

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports = {
         host: "containers-us-west-178.railway.app",
         user: "root",
         password: "wEWOgekoc9n9F5ETR2Je",
-        port: "7587",
+        port: 7587,
         database: "railway"
     }
 }
